5670bbc36da68eb59712401245fa0fd216dc1ae7
Will <Will>
2017-05-24 09:31:10 +1200

    fixed showArtSubjets()

--- se350_a3_submit/task_two/src/assets/js/wmol664.js
+++ se350_a3_submit/task_two/src/assets/js/wmol664.js
@@ -1,8 +1,17 @@
-function myFunction() {
-  var x = document.getElementById('myDIV');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
+function showArtSubjects() {
+  var x = document.getElementById('artSubjects');
+  if (x.style.display === 'block') {
     x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+function showSQOptions() {
+  var x = document.getElementById('sqOptions');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
   }
 }
