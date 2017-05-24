081ddf9e3654cef20bc09c82ca7b62cb38bee16b
will <willjoemolloy@gmail.com>
2017-05-24 19:29:24 +1200

    remove butotns from tabs

--- se350_a3_submit/task_two/src/assets/js/wmol664.js
+++ se350_a3_submit/task_two/src/assets/js/wmol664.js
@@ -1,10 +1,13 @@
 function showArtSubjects() {
   var x = document.getElementById('artSubjects');
+
   if (x.style.display === 'block') {
     x.style.display = 'none';
   } else {
     x.style.display = 'block';
   }
+
+
 }
 
 function showSQOptions() {
@@ -14,4 +17,22 @@ function showSQOptions() {
   } else {
     x.style.display = 'block';
   }
+}
+
+function showClassicalMusicTab(){
+	caller = $(classical_music)
+
+  console.log(caller)
+	caller.parent().find(".tab-controls").removeClass("active")
+	caller.addClass('active')
+
+	tab_ref = caller.attr('data-ref')
+	tab_group = caller.parent().attr('data-tabref')
+
+  console.log(caller)
+	console.log(tab_ref)
+	console.log(tab_group)
+
+	$("#" + tab_group + " .active-tab").removeClass("active-tab")
+	$("#" + tab_group + " #" + tab_ref).addClass("active-tab")
 }
